fa3dfaa4612af7c66437ec0b1ab2eec6cb496bd3
ivanrybkin <rybkin2517@mail.ru>
2023-11-22 16:56:15 +0300

    Initial Commit

---
+++ HH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HH
+//
+//  Created by Ivan Rybkin on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
